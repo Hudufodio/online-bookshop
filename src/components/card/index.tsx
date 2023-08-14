b3d412b36ddefa2b0fc4ba259d4e3c4922d41067
Hudu <ahmedhudu21@gmail.com>
2023-08-15 01:33:19 +0200

    api-fetch

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,14 +1,30 @@
 import './style.scss';
-const Card = () => {
+
+type Card = {
+	book: any;
+	map: any;
+};
+
+const Card = ({ book }: any) => {
+	console.log(book);
 	return (
 		<>
-			<div className="card">
-				<img src="./images/book1.jpeg" alt="" />
-				<div className="bottom">
-					<h3 className="title">React JS</h3>
-					<p className="amount">&#36; 15.00</p>
-				</div>
-			</div>
+			{book.map((item: any) => {
+				const thumbnail: any =
+					item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
+
+				return (
+					<>
+						<div className="card">
+							<img src={thumbnail} alt="" />
+							<div className="bottom">
+								<h3 className="title">React Js</h3>
+								<p className="amount">&#36; 15.00</p>
+							</div>
+						</div>
+					</>
+				);
+			})}
 		</>
 	);
 };
